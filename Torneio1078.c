710b73de35dc4e6dd865c72102bec4dcedb7ff73
juan4vieira <juan.vieira@unifesp.br>
2026-04-28 17:01:22 -0300

    Add multiplication table program in C
    Implement multiplication table for a given number.

---
+++ Torneio1078.c
@@ -0,0 +1,22 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Juan Mendes Vieira>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/runs/code/48784723
+Data        : 28/04/2026
+Objetivo    : Tabuada
+Aprendizado : <<<Exercício de fixação>>>
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main() {
+    int N, i;
+
+    scanf("%d", &N);
+
+    for(i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", i, N, i * N);
+    }
+
+    return 0;
+}
