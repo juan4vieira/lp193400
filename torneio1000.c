e101541b004945fa199c5db1157a7cbbd7c29abb
juan4vieira <juan.vieira@unifesp.br>
2026-04-28 16:30:25 -0300

    Add initial C program to print 'Hello World!'

---
+++ torneio1000.c
@@ -0,0 +1,16 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Juan Vieira>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 28/04/2026
+Objetivo    : Transparecer a mensagem "Hello World!"
+Aprendizado : <<<Aprendi a usufruir do comando "Print f,assim como os detalhes da programação>>>
+-------------------------------------------------------------------------- */
+
+int main() {
+ 
+    printf ("Hello World!\n");
+ 
+    return 0;
+}
