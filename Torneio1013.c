5ae9aa9b08aae3c36469fe8ea963d45fedd8b704
juan4vieira <juan.vieira@unifesp.br>
2026-04-28 23:49:16 -0300

    Implement program to find the largest of three integers
    This program reads three integers and determines the largest among them.

---
+++ Torneio1013.c
@@ -0,0 +1,24 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Juan Mendes Vieira>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1071
+Data        : DD/MM/2026
+Objetivo    : Ler dois valores inteiros e mostrar a soma.
+Aprendizado : <<<comandos scanf e E comercial>>>
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    int maiorAB, maior;
+
+    scanf("%d %d %d", &a, &b, &c);
+
+    maiorAB = (a + b + abs(a - b)) / 2;
+    maior = (maiorAB + c + abs(maiorAB - c)) / 2;
+
+    printf("%d eh o maior\n", maior);
+
+    return 0;
+}
