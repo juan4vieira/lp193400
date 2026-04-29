c2ba5bfac9f6668dbf96a3150c251847afb21a9a
juan4vieira <juan.vieira@unifesp.br>
2026-04-29 00:20:15 -0300

    Update torneio1000.c

--- torneio1000.c
+++ torneio1000.c
@@ -3,7 +3,7 @@ Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : <<<Juan Vieira>>>
 Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1000
-Data        : 28/04/2026
+Data        : 07/04/2026
 Objetivo    : Transparecer a mensagem "Hello World!"
 Aprendizado : <<<Aprendi a usufruir do comando "Print f,assim como os detalhes da programação>>>
 -------------------------------------------------------------------------- */
