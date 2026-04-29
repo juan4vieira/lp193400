598ef604eacd4b998e7820b36fee5a2aa495a537
juan4vieira <juan.vieira@unifesp.br>
2026-04-29 00:17:57 -0300

    Update Torneio1078.c

--- Torneio1078.c
+++ Torneio1078.c
@@ -2,7 +2,7 @@
 Disciplina  : Lógica de Programação, turma IB, 2026S1
 Nome        : <<<Juan Mendes Vieira>>>
 Linguagem   : C
-Problema    : https://judge.beecrowd.com/pt/runs/code/48784723
+Problema    : https://judge.beecrowd.com/pt/problems/view/1078
 Data        : 28/04/2026
 Objetivo    : Tabuada
 Aprendizado : <<<Exercício de fixação>>>
