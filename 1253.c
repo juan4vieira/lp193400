67286665f73843c8557da1da7c33f34afa2a9fc8
juan4vieira <juan.vieira@unifesp.br>
2026-06-18 14:37:11 -0300

    Implement Caesar cipher with alternating case

---
+++ 1253.c
@@ -0,0 +1,34 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Juan Mendes Vieira
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1253
+Data        : 18/06/2026
+Objetivo    : Cifra de César
+Aprendizado : <<<função dgets>>>
+Pergunta de segunda ordem: <<<Fazer a pergunta>>>
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main() {
+    char sentenca[100];
+
+    while (fgets(sentenca, sizeof(sentenca), stdin) != NULL) {
+        int maiuscula = 1;
+
+        for (int i = 0; sentenca[i] != '\0'; i++) {
+            if (isalpha(sentenca[i])) {
+                if (maiuscula) {
+                    sentenca[i] = toupper(sentenca[i]);
+                    maiuscula = 0;
+                } else {
+                    sentenca[i] = tolower(sentenca[i]);
+                    maiuscula = 1;
+                }
+            }
+        }
+        printf("%s", sentenca);
+    }
+
+    return 0;
+}
