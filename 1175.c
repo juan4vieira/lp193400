34697570f447e117b01b0a8bdba178c608d49a24
juan4vieira <juan.vieira@unifesp.br>
2026-06-23 16:21:40 -0300

    Add C program to reverse an array
    Implement a C program to reverse an array of integers.

---
+++ 1175.c
@@ -0,0 +1,32 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Juan Mendes Vieira>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1175
+Data        : 23/06/2026
+Objetivo    : trocar os vetores
+Aprendizado : <<<reforço de martrizes>>>
+Pergunta de segunda ordem: <<<Fazer a pergunta>>>
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+
+int main() {
+    int N[20];
+    int i, temp;
+
+    for (i = 0; i < 20; i++) {
+        scanf("%d", &N[i]);
+    }
+
+    for (i = 0; i < 10; i++) {
+        temp = N[i];
+        N[i] = N[19 - i];
+        N[19 - i] = temp;
+    }
+
+    for (i = 0; i < 20; i++) {
+        printf("N[%d] = %d\n", i, N[i]);
+    }
+
+    return 0;
+}
